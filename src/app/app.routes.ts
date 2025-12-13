5d1f54f7d7300b5cd4ff939e70f99459b0c7d8d6
Fateme Aslani <f.aslani2361@gmail.com>
2025-12-14 03:00:20 +0330

    Section1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './+public/+pages/login-page/login-page';
+import { PrivateTemplate } from './+private/private-template/private-template';
+import { BooksPage } from './+private/books-page/books-page';
+import { MembersPage } from './+private/members-page/members-page';
+import { ReportsPage } from './+private/reports-page/reports-page';
+import { BorrowsPage } from './+private/borrows-page/borrows-page';
+import { DashboardPage } from './+private/dashboard-page/dashboard-page';
+import { privateGuard } from './+shared/private-guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginPage },
+  {
+    path: 'private',canActivate:[privateGuard],component: PrivateTemplate,children: [
+      { path: 'books', component: BooksPage },
+      { path: 'members', component: MembersPage },
+      { path: 'reports', component: ReportsPage },
+      { path: 'borrows', component: BorrowsPage },
+      { path: 'dashboard', component: DashboardPage },
+      { path: '', redirectTo: 'dashboard', pathMatch: 'prefix' },
+      { path: '*', redirectTo: 'dashboard' },
+    ],
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '*', redirectTo: 'login' },
+];
